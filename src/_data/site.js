3749b800621c8362d99d0f90df642d382f90012c
Rodrigo Castro <18582098+rodrcastro@users.noreply.github.com>
2024-06-10 21:59:12 +0100

    update site description

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Rodrigo Castro",
   shortTitle: "Rodrigo Castro",
   description:
-    "A personal website and blog for Rodrigo Castro. Made with 💙, powered by Eleventy and using the Nulite theme",
+    "A personal website and blog for Rodrigo Castro. Made with 💙 with Eleventy and using the Nulite theme base",
   url: "https://rodrcastro.com/",
   author: {
     name: "Rodrigo Castro",
