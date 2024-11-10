d5d309490e18f9241cd864e080db374ffc766419
Rodrigo Castro <18582098+rodrcastro@users.noreply.github.com>
2024-11-10 18:09:35 +0000

    [CI Skip] updating links

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Rodrigo Castro",
   shortTitle: "Rodrigo Castro",
   description:
-    "A personal website and blog for Rodrigo Castro. Made with 💙 with Eleventy and using the Nulite theme base",
+    "O espaço de Rodrigo Castro na internet.",
   url: "https://rodrcastro.com/",
   author: {
     name: "Rodrigo Castro",
