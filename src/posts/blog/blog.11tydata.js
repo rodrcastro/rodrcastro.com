47f554ddbad9f0aa69063305102914910c2024fb
Rodrigo Castro <18582098+rodrcastro@users.noreply.github.com>
2025-10-09 12:07:28 +0100

    specify header links property in pages and blogs

--- src/posts/blog/blog.11tydata.js
+++ src/posts/blog/blog.11tydata.js
@@ -2,4 +2,5 @@ module.exports = {
   // Set a default layout for everything in the src folder and below.
   layout: "layouts/post.liquid",
   permalink: "blog/{{ page.fileSlug }}/index.html",
+  showHeadingAnchors: true,
 };
--- src/posts/pages/pages.11tydata.js
+++ src/posts/pages/pages.11tydata.js
@@ -2,4 +2,5 @@ module.exports = {
   // Set a default layout for everything in the src folder and below.
   layout: "layouts/post.liquid",
   permalink: "{{ page.fileSlug }}/index.html",
+  showHeadingAnchors: false,
 };
